153339e69ad3b7d503a976c0e7db7fbb1ed982de
John.Chang <chang.yuchuan@rakuten.com>
2021-09-06 23:42:49 +0800

    turn on connectivity panel

--- app/src/main/java/com/example/androidexample/MainActivity.kt
+++ app/src/main/java/com/example/androidexample/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.androidexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val startNetworkSettingForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.btn.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                startNetworkSettingForResult.launch(Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY))
+            } else {
+                startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))
+            }
+        }
+        setContentView(binding.root)
     }
 }
