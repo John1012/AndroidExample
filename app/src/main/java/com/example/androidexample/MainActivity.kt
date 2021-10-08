bf89496dc14fd3a9009ee0410da790bd84417032
John.Chang <ddtddt55@gmail.com>
2021-10-09 04:22:43 +0800

    Show content by Compose

--- app/src/main/java/com/example/androidexample/MainActivity.kt
+++ app/src/main/java/com/example/androidexample/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.androidexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.lifecycleScope
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidexample.remote.PostsService
-import kotlinx.coroutines.launch
+import com.example.androidexample.remote.dao.PostResponse
+import com.example.ktorexample.ui.theme.AndroidExampleTheme
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,12 +23,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        lifecycleScope.launch {
-            val posts = postsService.getPosts()
-            posts.forEach {
-                Log.d("John", "Post: $it")
+        setContent {
+            val posts = produceState<List<PostResponse>>(
+                initialValue = emptyList(),
+                producer = {
+                    value = postsService.getPosts()
+                }
+            )
+            AndroidExampleTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    LazyColumn {
+                        items(posts.value) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            ) {
+                                Text(text = it.title, fontSize = 20.sp)
+                                Spacer(modifier = Modifier.height(4.dp))
+                                Text(text = it.body, fontSize = 16.sp)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 }
+
