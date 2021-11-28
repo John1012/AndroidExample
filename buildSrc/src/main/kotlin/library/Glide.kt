98be5c792278f218b004dac112c80410954eac1d
John.Chang <ddtddt55@gmail.com>
2021-11-28 10:58:30 +0800

    Add Glide Library

---
+++ buildSrc/src/main/kotlin/library/Glide.kt
@@ -0,0 +1,7 @@
+package library
+
+object Glide {
+    private const val glideVersion = "4.9.0"
+    const val glide = "com.github.bumptech.glide:glide:$glideVersion"
+    const val processor = "com.github.bumptech.glide:compiler:$glideVersion"
+}
