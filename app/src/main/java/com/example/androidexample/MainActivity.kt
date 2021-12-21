36875f000d3560b7aacf36f1b01e1bd6afdb52e4
John.Chang <ddtddt55@gmail.com>
2021-12-21 23:34:12 +0800

    Build template

--- app/src/main/java/com/example/androidexample/MainActivity.kt
+++ app/src/main/java/com/example/androidexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidexample
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.lifecycle.lifecycleScope
 import com.example.androidexample.remote.PostsService
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,20 +13,11 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var httpClient: HttpClient
-
-    private val postsService: PostsService by lazy {
-        PostsService.create(httpClient)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launch {
-            val posts = postsService.getPosts()
-            posts.forEach {
-                Log.d("John", "Post:$it")
-            }
+        setContent {
+
         }
     }
 }
